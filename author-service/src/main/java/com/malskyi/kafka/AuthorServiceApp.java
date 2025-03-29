554db1e3c3a4e65a8e96f2aa00f5945e20dc5ff4
Yurii Malskyi <yurii.maslkyi@gmail.com>
2025-03-29 20:27:40 +0200

    Cleaned up main classes

--- author-service/src/main/java/com/malskyi/kafka/AuthorServiceApp.java
+++ author-service/src/main/java/com/malskyi/kafka/AuthorServiceApp.java
@@ -1,32 +1,16 @@
 package com.malskyi.kafka;
 
-import lombok.extern.log4j.Log4j2;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@Log4j2
 @EnableAsync
 @EnableScheduling
 @SpringBootApplication
-public class AuthorServiceApp implements CommandLineRunner {
-
-    @Value("${logging.file.name}")
-    private String loggingFileName;
-    @Value("${logging.file.pattern}")
-    private String loggingFilePattern;
+public class AuthorServiceApp {
 
     public static void main(String[] args) {
         SpringApplication.run(AuthorServiceApp.class, args);
-    }
-
-    @Override
-    public void run(String... args) throws Exception {
-        log.debug("loggingFileName: {}", loggingFileName);
-        log.debug("loggingFilePattern: {}", loggingFilePattern);
     }
 }
